8adfcadf92ef27b4b5e3326c4aa33bd9783bea70
Andrew Naschokin <38532956+Dragen228@users.noreply.github.com>
2023-07-21 16:49:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,28 @@
-self.addEventListener('install', function(event) {
-const indexPage = new Request('https://dragen228.github.io/naschokin');
-event.waitUntil(
-fetch(indexPage).then(function(response) {
-return caches.open('pwabuilder-offline').then(function(cache) {
-console.log('[PWA Builder] Cached index page during Install'+ response.url);
-return cache.put(indexPage, response);
-});
-}));
-});
-self.addEventListener('fetch', function(event) {
-const updateCache = function(request){
-return caches.open('pwabuilder-offline').then(function (cache) {
-return fetch(request).then(function (response) {
-console.log('[PWA Builder] add page to offline'+response.url)
-return cache.put(request, response);
-});
-});
-};
-event.waitUntil(updateCache(event.request));
-event.respondWith(
-fetch(event.request).catch(function(error) {
-console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
-return caches.open('pwabuilder-offline').then(function (cache) {
-return cache.match(event.request).then(function (matching) {
-var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-return report
-});
-});
+const staticCacheName = 's-app-v1'
+
+const assertUrls = [
+	'index.html',
+	'app.js',
+	'/css/main.css'
+]
+
+self.addEventListener('install', async event => {
+	const cache = await caches.open(staticCacheName)
+	await cache.addAll(assertUrls)
 })
-);
+
+self.addEventListener( 'install', event => {
+	event.waitUntil(
+		caches.open(staticCacheName).then(cache => cache.addAll(assertUrls)))
 })
+
+self.addEventListener('activate', event => {
+	concole.log('[sw]: activate')
+})
+
+async function cacheFirst(request) {
+	const cached = await caches.match(request)
+	return cached ?? await fetch(request)
+}
+
+const url = new URL(request )
